d87c2d0a0e1cd75c3cadd7ea3b2876d341f568f9
Łukasz Złotnik <79901733+zlotnikluk3@users.noreply.github.com>
2022-04-02 12:29:28 +0200

    Update DBuild.java

--- app/src/main/java/logic/DBuild.java
+++ app/src/main/java/logic/DBuild.java
@@ -18,8 +18,7 @@ public class DBuild {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         if (conn == null || conn.isClosed()) {
-            conn = DriverManager.getConnection("jdbc:postgresql://ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d1v1eetd9rgfsh?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
-                    "mnwvocrgecchbs", "e0fbc54ffe89e6bb12b8ca3e8e6971143b97d97bded509b81afe492cfacbdaee");
+            conn = DriverManager.getConnection("xxx");
         }
         return conn;
     }
